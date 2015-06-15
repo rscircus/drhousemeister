c9537578e23d1979786ce36ae66c0ce91e8f2cc0
Roland Siegbert <siegbert@cats.rwth-aachen.de>
2015-06-15 14:06:59 +0200

    Add databases and clean up code

--- filesort.py
+++ filesort.py
@@ -3,10 +3,10 @@
 import os
 import shutil as sh
 
+# verbose output:
 debug = False
 
 def helper():
-
     print ""
     print "Usage: python filesort.py [OPTION] [DIR]"
     print "Sort files from DIR (the current directory by default) into"
@@ -14,14 +14,6 @@ def helper():
     print ""
     print "This program creates folders containing files of one kind."
     print "With the kind defining the folder's name."
-#    print ""
-#    print "Optionally the files can further be sorted by year."
-#    print ""
-
-
-#
-# MAIN
-#
 
 # inform the user:
 helper()
@@ -31,7 +23,7 @@ direntries = os.listdir('./')
 
 #
 # Create list of file extensions - with corresponding filenames
-
+#
 # Sort out directories:
 files = []
 for direntry in direntries:
@@ -62,59 +54,66 @@ print ""
 #
 
 # This is pretty hacky. Most file managers sort the _ ahead of everything else
-docs = {'.tex', '.doc', '.dot', '.docm', '.docx', '.rtf', '.odt', '.odm', '.ott', '.txt'}
-spreadsheets = {'.ods', 'ots', '.xls', '.xlsx', '.csv'}
-images = {'.png', '.gif', '.jpg', '.jpeg', '.svg', '.xcf'}
 archives = {'.zip', '.gz', '.tgz', '.rar' }
+databases = {'.sql', '.json'}
+docs = {'.tex', '.doc', '.dot', '.docm', '.docx', '.rtf', '.odt', '.odm', '.ott', '.txt'}
+images = {'.png', '.gif', '.jpg', '.jpeg', '.svg', '.xcf'}
+spreadsheets = {'.ods', 'ots', '.xls', '.xlsx', '.csv'}
+pdfs = {'.pdf', '.skim'}
 presentations = {'.ppt', '.pptx', '.odp', '.otp', '.pez', '.keynote'}
 #videos = {'.avi', '.mp4' '.mpg', '.mkv'}
-pdfs = {'.pdf', '.skim'}
-
-for doc in docs:
-    if doc in extensions:
-        if not os.path.exists('_documents'):
-            os.mkdir('_documents')
-
-for spreadsheet in spreadsheets:
-    if spreadsheet in extensions:
-        if not os.path.exists('_spreadsheets'):
-            os.mkdir('_spreadsheets')
-
-for image in images:
-    if image in extensions:
-        if not os.path.exists('_images'):
-            os.mkdir('_images')
 
 for archive in archives:
     if archive in extensions:
         if not os.path.exists('_archives'):
             os.mkdir('_archives')
 
-for presentation in presentations:
-    if presentation in extensions:
-        if not os.path.exists('_presentations'):
-            os.mkdir('_presentations')
+for database in databases:
+    if database in extensions:
+        if not os.path.exists('_databases'):
+            os.mkdir('_databases')
+
+for doc in docs:
+    if doc in extensions:
+        if not os.path.exists('_documents'):
+            os.mkdir('_documents')
+
+for image in images:
+    if image in extensions:
+        if not os.path.exists('_images'):
+            os.mkdir('_images')
+
+for spreadsheet in spreadsheets:
+    if spreadsheet in extensions:
+        if not os.path.exists('_spreadsheets'):
+            os.mkdir('_spreadsheets')
 
 for pdf in pdfs:
     if pdf in extensions:
         if not os.path.exists('_pdfs'):
             os.mkdir('_pdfs')
 
+for presentation in presentations:
+    if presentation in extensions:
+        if not os.path.exists('_presentations'):
+            os.mkdir('_presentations')
+
+
 
 # Move files into dirs
 for file in files:
-    if file[1].lower() in docs:
-        sh.move(file[0]+file[1], '_documents/'+file[0]+file[1])
-    if file[1].lower() in spreadsheets:
-        sh.move(file[0]+file[1], '_spreadsheets/'+file[0]+file[1])
-    if file[1].lower() in images:
-        sh.move(file[0]+file[1], '_images/'+file[0]+file[1])
     if file[1].lower() in archives:
         sh.move(file[0]+file[1], '_archives/'+file[0]+file[1])
+    if file[1].lower() in databases:
+        sh.move(file[0]+file[1], '_databases/'+file[0]+file[1])
+    if file[1].lower() in docs:
+        sh.move(file[0]+file[1], '_documents/'+file[0]+file[1])
+    if file[1].lower() in images:
+        sh.move(file[0]+file[1], '_images/'+file[0]+file[1])
+    if file[1].lower() in spreadsheets:
+        sh.move(file[0]+file[1], '_spreadsheets/'+file[0]+file[1])
     if file[1].lower() in presentations:
         sh.move(file[0]+file[1], '_presentations/'+file[0]+file[1])
-    if file[1].lower() in pdfs:
-        sh.move(file[0]+file[1], '_pdfs/'+file[0]+file[1])
 
 
 #TODO: If more than one "year" present than create folders with year
